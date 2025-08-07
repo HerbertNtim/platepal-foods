aa89f11a3516bd2e01ed1a9ae2b365edb8d74b12
HerbertNtim <herbertntim15@gmail.com>
2025-08-07 11:43:52 +0000

    working auth page

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,16 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { Slot } from 'expo-router'
+import { images } from '@/constants'
 
 const _LayoutAuth = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>_LayoutAuth</Text>
-      </View>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} >
+      <ScrollView className="bg-white h-full" style={{ height: Dimensions.get('screen').height / 2.25 }}>
+        <View>
+          <ImageBackground source={images.loginGraphic} className='size-full rounded-b-lg' resizeMode='stretch' />
+          <Image source={images.logo} className='self-center size-48 absolute -bottom-16 z-10' />
+        </View>
+      </ScrollView>
       <Slot />
-    </SafeAreaView>
+    </KeyboardAvoidingView>
   )
 }
 
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.platepal",
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: '68946b430017898dc57c',
+  userCollectionId: '68946bf50021952e3ceb'
 }
