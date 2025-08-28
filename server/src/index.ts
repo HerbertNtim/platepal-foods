0ab6abbd55b5333d30f714375ccdad67b06a6aab
HerbertNtim <herbertntim15@gmail.com>
2025-08-28 17:16:28 +0000

    setup server

---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+// ROUTES 
+
+// CONFIGURATIONS
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan('common'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// SERVER
+const PORT = process.env.PORT || 5000;
+if (!isProduction) {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
